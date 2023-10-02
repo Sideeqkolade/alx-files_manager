283d6fddce52e923cd0863a5e8f549bf90b7808e
Sideeqkolade <sideeqkolade@gmail.com>
2023-10-02 21:51:50 +0100

    task 1 completed

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    //MongoDB connection URL
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    // create a mongoDB client
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+
+    // connect to a mongodDB server
+    this.client.connect();
+  }
+
+  // Check if the connection to MongoDB is alive
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  // Asynchronous function to get the number of documents in the "users" collection
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  // Asynchronous function to get the number of documents in the "files" collection
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
